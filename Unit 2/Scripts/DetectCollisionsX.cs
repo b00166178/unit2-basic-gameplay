73d2127a49c4d30be0e58e73abd8c4ebdf5ec65e
b00166178 <b00166178@mytudublin.ie>
2024-10-14 20:29:43 +0100

    Add files

---
+++ Unit 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+		//destroy the ball when comes in contact with the dog
+		
+        Destroy(gameObject);
+
+    }
+	
+}
---
+++ Unit 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+	// time the player has to wait to fire again
+	private float fireRate = 1; 
+	// time since start after which player can fire again
+	private float nextFire = 0; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if enough time has elapsed since last fire, send dog send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+		// reset nextFire to current time + fireRate
+		nextFire = Time.time + fireRate; 
+ 		Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+		}
+
+    }
+}
